86ee9681f2beadfe93231b6c431a724d1b14bdf1
mupt <fmbonnevier@gmail.com>
2022-12-25 20:43:31 -0500

    Return link to correct one.

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -2,7 +2,7 @@
   <nav>
     <ol>
       <li>
-        <a class="text-lg" href="https://www.instagram.com/kidclampdown/"> sinstagram </a>
+        <a class="text-lg" href="https://www.instagram.com/kidclampdown/"> instagram </a>
       </li>
       <li>
         <a class="text-lg" href="https://onecouldargue.substack.com/"> newsletter </a>
